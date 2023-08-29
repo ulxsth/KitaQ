4ad4b036cb40458d0924811b38201fdff28011af
isimatu0409 <136344947+isimatu0409@users.noreply.github.com>
2023-08-29 18:17:45 +0900

    add: html作成

---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../../css/user/register.blade.css">
+</head>
+<body >
+  <form action="#" method="post">
+    <div class="Yuzer">
+
+    <p>ログイン</p>
+
+    <p><input type="text" name="yuzr" required placeholder="ユーザー"></p>
+
+    <p><input type="text" name="ps" required placeholder="パスワード"></p>
+
+    <br>
+
+    <p><input type="submit" name="submit" value="ログイン"></p>
+    </div>
+</form>
+<form action="#" method="post">
+    <div class="NewYuzer">
+
+    <p>初めの方はこちら</p>
+
+    <p><input type="text" name="yuzr" required placeholder="ユーザー"></p>
+
+    <p> <input type="text" name="ps" required placeholder="パスワード"></p>
+
+    <p><input type="checkbox" name="kigyou" value="企業のアカウントとして登録">企業のアカウントとして登録</p>
+
+    <p><input type="submit" name="submit" value="新規登録"></p>
+    </div>
+  </form>
+</body>
+</html>
