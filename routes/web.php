fefef23a0b5882da33ffb60f3bea9738adc22c35
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-30 06:54:40 +0900

    update: wip

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.register_login');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
