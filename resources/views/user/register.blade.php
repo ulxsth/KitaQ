b854f3ff4eda14e193e66f09a9d868a3d8ec1f13
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-08-29 21:03:13 +0900

    Update register.blade.php

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="../../css/user/register.blade.css">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../../css/user/reset.css">
+    <link rel="stylesheet" href="../../css/user/style_header.css">
+    <link rel="stylesheet" href="../../css/user/register.blade.css">
+    <script src="../../js/header.js"></script>
 </head>
-<body >
-  <form action="#" method="post">
-    <div class="Yuzer">
 
-    <p>ログイン</p>
-
-    <p><input type="text" name="yuzr" required placeholder="ユーザー"></p>
-
-    <p><input type="text" name="ps" required placeholder="パスワード"></p>
-
-    <br>
-
-    <p><input type="submit" name="submit" value="ログイン"></p>
+<body>
+    <div class="login__wrap">
+        <form action="#" method="post">
+            <div class="user">
+                <p class="login">ログイン</p>
+                <input type="text" name="user" required placeholder="ユーザー名" class="txt">
+                <input type="text" name="pass" required placeholder="パスワード" class="txt">
+                <div class="center">
+                    <input type="submit" name="submit" value="ログイン" class="submit">
+                </div>
+            </div>
+        </form>
+        <form action="#" method="post">
+            <div class="user">
+                <p class="login">はじめての方はこちら</p>
+                <input type="text" name="user" required placeholder="ユーザー" class="txt">
+                <input type="text" name="pass" required placeholder="パスワード" class="txt">
+                <label class="rdo"><input type="radio" name="enterprise" onclick="checkRdo(this, 1)">企業アカウントとして登録</label>
+                <div class="center">
+                    <input type="submit" name="submit" value="新規登録" class="submit">
+                </div>
+            </div>
+        </form>
     </div>
-</form>
-<form action="#" method="post">
-    <div class="NewYuzer">
-
-    <p>初めの方はこちら</p>
-
-    <p><input type="text" name="yuzr" required placeholder="ユーザー"></p>
-
-    <p> <input type="text" name="ps" required placeholder="パスワード"></p>
-
-    <p><input type="checkbox" name="kigyou" value="企業のアカウントとして登録">企業のアカウントとして登録</p>
-
-    <p><input type="submit" name="submit" value="新規登録"></p>
-    </div>
-  </form>
 </body>
+
 </html>
