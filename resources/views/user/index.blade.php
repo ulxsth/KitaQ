835082ff765933346935b3c56fb0d3496ed409ae
isimatu0409 <136344947+isimatu0409@users.noreply.github.com>
2023-08-30 11:37:42 +0900

    feature-#21

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<link href="../../css/user/index.blade.css" rel="stylesheet" />
+
+<body>
+<div style="text-align:center;">
+        <iframe
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.309945400945!2d130.87649217559584!3d33.88167057322172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3543bf52dd6aec29%3A0x294945965d398093!2z44CSODAyLTAwNzcg56aP5bKh55yM5YyX5Lmd5bee5biC5bCP5YCJ5YyX5Yy66aas5YCf77yR5LiB55uu77yV4oiS77yS77yVIOODm-ODqeODpOODk-ODqyA0Zg!5e0!3m2!1sja!2sjp!4v1693359604274!5m2!1sja!2sjp" 
+         width="600"
+          height="450" 
+         style="border:0;"
+          allowfullscreen="" 
+         loading="lazy" 
+         referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+
+</div>
+</body>
+</html>
