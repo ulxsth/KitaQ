2eaddd164f23d1961462d05345151bfce27e826a
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-30 12:41:12 +0900

    update

--- resources/views/user/register_login.blade.php
+++ resources/views/user/register_login.blade.php
@@ -14,22 +14,24 @@
 <body>
     @include('templates.header')
     <div class="login__wrap">
-        <form action="#" method="post">
+        <form action="{{ route("login") }}" method="post">
+            @csrf
             <div class="user">
                 <p class="login">ログイン</p>
-                <input type="email" name="mail" required placeholder="メールアドレス" class="txt">
-                <input type="text" name="pass" required placeholder="パスワード" class="txt">
+                <input type="email" name="email" required placeholder="メールアドレス" class="txt">
+                <input type="password" name="password" required placeholder="パスワード" class="txt">
                 <div class="center">
                     <input type="submit" name="submit" value="ログイン" class="submit">
                 </div>
             </div>
         </form>
-        <form action="#" method="post">
+        <form action="{{ route('register') }}" method="post">
+            @csrf
             <div class="user">
                 <p class="login">はじめての方はこちら</p>
-                <input type="email" name="mail" required placeholder="メールアドレス" class="txt">
-                <input type="text" name="user" required placeholder="ユーザー名" class="txt">
-                <input type="text" name="pass" required placeholder="パスワード" class="txt">
+                <input type="email" name="email" required placeholder="メールアドレス" class="txt">
+                <input type="text" name="name" required placeholder="ユーザー名" class="txt">
+                <input type="password" name="password" required placeholder="パスワード" class="txt">
                 <label class="rdo"><input type="checkbox" name="enterprise">企業アカウントとして登録</label>
                 <div class="center">
                     <input type="submit" name="submit" value="新規登録" class="submit">
