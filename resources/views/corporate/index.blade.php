832cff7c7c97e5d361b9f3c1a4299beb7bd41a3d
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2023-08-30 12:52:56 +0900

    Create index.blade.php

---
+++ resources/views/corporate/index.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8" />
+    <link href="../../../resources/css/user/index.blade.css" rel="stylesheet" />
+</head>
+
+<body>
+    <table class="menyu" id="MN" border="2" width="auto">
+
+        <tr>
+            <th rowspan="4"> <a href="../../../resources/img/no-image.jpg"><img class="syasinn" alt="no-imeage" src="../../../resources/img/no-image.jpg"></a></th>
+            <td>企業名</td>
+            <td>ハッカソン</td>
+        </tr>
+        <tr>
+            <td>場所</td>
+            <td>〒802-0077 福岡県北九州市小倉北区馬借１丁目５−２５ ホラヤビル </td>
+        </tr>
+        <tr>
+            <th><a>URL</a></th>
+            <th><a href="https://photos.app.goo.gl/kTYw7XLrSX21xjMx8">https://photos.app.goo.gl/kTYw7XLrSX21xjMx8</a></tb>
+        </tr>
+        <tr>
+            <th>説明</th>
+            <td></td>
+        </tr>
+    </table>
+    <p></p>
+
+    <table class="menyu" id="MN" border="2" width="auto">
+
+        <tr>
+            <th rowspan="4"> <a href="../../../resources/img/no-image.jpg"><img class="syasinn" alt="no-imeage" src="../../../resources/img/no-image.jpg"></a></th>
+            <td>企業名</td>
+            <td>ハッカソン</td>
+        </tr>
+        <tr>
+            <td>場所</td>
+            <td>〒802-0077 福岡県北九州市小倉北区馬借１丁目５−２５ ホラヤビル </td>
+        </tr>
+        <tr>
+            <th><a>URL</a></th>
+            <th><a href="https://photos.app.goo.gl/kTYw7XLrSX21xjMx8">https://photos.app.goo.gl/kTYw7XLrSX21xjMx8</a></tb>
+        </tr>
+        <tr>
+            <th>説明</th>
+            <td></td>
+        </tr>
+    </table>
+    <p></p>
+
+    <table class="menyu" id="MN" border="2" width="auto">
+
+        <tr>
+            <th rowspan="4"> <a href="../../../resources/img/no-image.jpg"><img class="syasinn" alt="no-imeage" src="../../../resources/img/no-image.jpg"></a></th>
+            <td>企業名</td>
+            <td>ハッカソン</td>
+        </tr>
+        <tr>
+            <td>場所</td>
+            <td>〒802-0077 福岡県北九州市小倉北区馬借１丁目５−２５ ホラヤビル </td>
+        </tr>
+        <tr>
+            <th><a>URL</a></th>
+            <th><a href="https://photos.app.goo.gl/kTYw7XLrSX21xjMx8">https://photos.app.goo.gl/kTYw7XLrSX21xjMx8</a></tb>
+        </tr>
+        <tr>
+            <th>説明</th>
+            <td></td>
+        </tr>
+    </table>
+    <p></p>
+
+    <table class="menyu" id="MN" border="2" width="auto">
+
+        <tr>
+            <th rowspan="4"> <a href="../../../resources/img/no-image.jpg"><img class="syasinn" alt="no-imeage" src="../../../resources/img/no-image.jpg"></a></th>
+            <td>企業名</td>
+            <td>ハッカソン</td>
+        </tr>
+        <tr>
+            <td>場所</td>
+            <td>〒802-0077 福岡県北九州市小倉北区馬借１丁目５−２５ ホラヤビル </td>
+        </tr>
+        <tr>
+            <th><a>URL</a></th>
+            <th><a href="https://photos.app.goo.gl/kTYw7XLrSX21xjMx8">https://photos.app.goo.gl/kTYw7XLrSX21xjMx8</a></tb>
+        </tr>
+        <tr>
+            <th>説明</th>
+            <td></td>
+        </tr>
+    </table>
+    <p></p>
+</body>
+
+</html>
