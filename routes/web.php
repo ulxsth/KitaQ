b088c9a781abe92631c9e85a07fe3d362000cf58
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-30 13:35:05 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,14 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get("/corporate/index", function() {
+        return view("corporate.index");
+    })->name("corporate.index");
+
+    // for demo
+    Route::get("/property/demo", function () {
+        return view("property.index");
+    })->name("property.index");
 });
 
 require __DIR__.'/auth.php';
